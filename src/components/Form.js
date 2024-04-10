4d622bbea4895ea53943edebbd6f08200b0ce722
vinay chaurasia <vinay84ia1@gmail.com>
2024-04-10 16:50:39 +0530

    change in ui design

--- src/components/Form.js
+++ src/components/Form.js
@@ -33,14 +33,13 @@ export default function Form() {
       console.log(values);
     },
   });
-  console.log(formik.touched);
   console.log(formik.errors);
   return (
     <div className="">
-      <div className="text-center bg-yellow-100 py-2  my-2 text-lg font-bold mb-8">
+      <div className="text-center bg-slate-100 py-4  my-2 text-lg font-bold mb-8">
         Form handling
       </div>
-      <div className="flex flex-col text-center bg-slate-400 w-5/12 ml-auto mr-auto">
+      <div className="flex flex-col text-center bg-slate-50 w-5/12 ml-auto mr-auto mt-32">
         <form onSubmit={formik.handleSubmit}>
           <div>
             <label className="inline-block w-32 text-left font-bold ">
@@ -52,7 +51,7 @@ export default function Form() {
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.name}
-              className="p-1 mx-3 mt-12 w-60"
+              className="p-1 mx-3 mt-12 w-60 bg-zinc-200 rounded-md"
             ></input>
             {formik.touched.name && formik.errors.name ? (
               <div className=" text-red-600  ml-[260px] text-left ">
@@ -72,7 +71,7 @@ export default function Form() {
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.number}
-              className="p-1 mx-3 w-60"
+              className="p-1 mx-3 w-60  bg-zinc-200 rounded-md"
             ></input>
             {formik.touched.number && formik.errors.number ? (
               <div className=" text-red-600  ml-[260px] text-left ">
@@ -92,7 +91,7 @@ export default function Form() {
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               value={formik.values.email}
-              className="p-1 mx-3  w-60"
+              className="p-1 mx-3  w-60  bg-zinc-200 rounded-md"
             ></input>
             {formik.touched.email && formik.errors.email ? (
               <div className=" text-red-600  ml-[260px] text-left ">
@@ -103,7 +102,11 @@ export default function Form() {
             )}
           </div>
           <div>
-            <input type="submit" value="submit" className="mb-12 mt-4"></input>
+            <input
+              type="submit"
+              value="submit"
+              className="mb-12 mt-4 bg-zinc-600 font-bold text-white p-2 px-4 rounded-lg"
+            ></input>
           </div>
         </form>
       </div>
